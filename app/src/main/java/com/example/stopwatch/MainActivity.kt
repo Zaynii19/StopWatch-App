11dd074f180a513a9f28fc1506ccade1125d885e
ALI ZAIN <zaynii1911491@gmail.com>
2024-05-15 12:09:44 +0500

    beta release

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.stopwatch
 
 import android.app.Dialog
+import android.graphics.Color
 import android.os.Bundle
 import android.os.SystemClock
 import android.widget.ArrayAdapter
@@ -52,6 +53,7 @@ class MainActivity : AppCompatActivity() {
             val numberPicker = dialog.findViewById<NumberPicker>(R.id.numberPicker)
             numberPicker.minValue = 0
             numberPicker.maxValue = 5
+            numberPicker.textColor = Color.BLACK
 
             //Set Time from dialog to Time text
             val setTimeBtn = dialog.findViewById<Button>(R.id.setTime)
@@ -66,36 +68,43 @@ class MainActivity : AppCompatActivity() {
             dialog.show()
         }
 
+        var isRunning = false
+
         binding.stoper.setOnClickListener {
-            if (!isRunning){
+            if (isRunning) {
+                binding.countDown.stop()
                 isRunning = false
-                if (!minutes.equals("00:00:00")){
-                    val totalMin = minutes!!.toInt() * 60 *1000L  //60 sec and 1000 milisec
-                    binding.countDown.base = SystemClock.elapsedRealtime() + totalMin  //Set Timer Countdown Base
-                    binding.countDown.format = "%S %S"
+                binding.stoper.text = buildString { append("RUN") }
+            } else {
+                isRunning = true
+                if (!minutes.equals("00:00:00")) {
+                    val totalMin = minutes!!.toInt() * 60 * 1000L  //60 sec and 1000 milisec
+                    // Reset base time when starting or restarting countdown
+                    binding.countDown.base = SystemClock.elapsedRealtime()
+                    binding.countDown.format = "%M:%S" // Update format to show minutes and seconds
                     binding.countDown.onChronometerTickListener = Chronometer.OnChronometerTickListener {
-                        val elapsedTime = SystemClock.elapsedRealtime() - binding.countDown.base //Decrease time by 1 sec
-                        //stop when set time reaches
-                        if (elapsedTime>=totalMin){
+                        val elapsedTime = SystemClock.elapsedRealtime() - binding.countDown.base // Decrease time by 1 sec
+                        val remainingTime = totalMin - elapsedTime
+                        // Update chronometer with remaining time in minutes and seconds
+                        binding.countDown.text = String.format("%02d:%02d", remainingTime / (1000 * 60), remainingTime % (1000 * 60) / 1000)
+                        // Stop when timer reaches zero
+                        if (remainingTime <= 0) {
                             binding.countDown.stop()
                             isRunning = false
-                            binding.stoper.text = "RUN"
+                            binding.stoper.text = buildString { append("RUN") }
                         }
                     }
-                }else{
+                    binding.stoper.text = buildString { append("STOP") } // Update button text to "STOP" when starting countdown
+                    binding.countDown.start()
+                } else {
                     isRunning = true
                     binding.countDown.base = SystemClock.elapsedRealtime() //stop decreasing/increasing beyond limit
-                    binding.stoper.text = "STOP"
+                    binding.stoper.text = buildString { append("STOP") }
                     binding.countDown.start()
                 }
-            }else{
-                isRunning = true
-                binding.countDown.base = SystemClock.elapsedRealtime() //stop decreasing/increasing beyond limit
-                binding.stoper.text = "STOP"
             }
-
-            binding.countDown.start()
         }
+
 
         //laps List
         binding.lapList.adapter = arrayAdapter
